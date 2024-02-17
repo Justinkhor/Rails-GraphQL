c29d322606b71c2f8301fae6a2d137b54e53a93b
justinkhor <justin@adra.ai>
2024-02-18 01:09:41 +0800

    Update deployment config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,7 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
+
+  # Add deployment host
+  config.hosts << "rails-graphql-aj16.onrender.com"
 end
